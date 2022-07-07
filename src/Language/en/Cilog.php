c38d174b03f2a05c9789995a9b6f856d02a3244c
Official XVIID <100110814+officialxviid@users.noreply.github.com>
2022-07-07 08:19:49 +0700

    Create Cilog.php

---
+++ src/Language/en/Cilog.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'missingDatabaseTable'  => 'Table "{0}" missing.',
+    'couldntCreateDir'      => 'Could not create directory in "{0}".',
+    'couldntCreateFile'     => 'Could not create file "{0}".',
+    'errorReadingFile'      => 'Error reading file: "{0}".',
+    'unavailableMethod'     => 'The "{0}" method is not available for remote JSON files.',
+];
